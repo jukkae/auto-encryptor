78969b137e4e03504d4aeda3615be7b65fb9b96e
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-10-17 15:37:01 +0300

    watch logic

--- AutoEncryptor/src/AutoEncryptor.java
+++ AutoEncryptor/src/AutoEncryptor.java
@@ -9,21 +9,51 @@ import java.io.*;
 import java.util.*;
 
 public class AutoEncryptor {
+	private WatchService watcher;
+	private Map<WatchKey, Path> keys;
+	private boolean trace = false;
+	private Path remoteDir;
+	private Path directory; // TODO this is for testing, use directory registry!
 
-	public AutoEncryptor(Path dir, Path remoteDir) {
-		// TODO Auto-generated constructor stub
+	public AutoEncryptor(Path dir, Path remoteDir) throws IOException {
+		this.watcher = FileSystems.getDefault().newWatchService();
+		this.keys = new HashMap<WatchKey, Path>();
+		this.remoteDir = remoteDir;
+		this.directory = dir;
+
+		register(dir);
+
+		// enable trace after initial registration
+		this.trace = true;
+	}
+
+	private void register(Path dir) throws IOException {
+		// TODO Auto-generated method stub
+		WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE,
+				ENTRY_MODIFY);
+		if (trace) {
+			Path prev = keys.get(key);
+			if (prev == null) {
+				System.out.format("register: %s\n", dir);
+			} else {
+				if (!dir.equals(prev)) {
+					System.out.format("update: %s -> %s\n", prev, dir);
+				}
+			}
+		}
+		keys.put(key, dir);
 	}
 
 	private void processEvents() {
 		// TODO Auto-generated method stub
-
+		System.out.println("test");
 	}
 
 	public static void usage() {
 		System.out.println("Usage information:");
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// parse arguments
 		if (args.length != 2)
 			usage();
