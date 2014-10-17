5219531d552c56216708c469a1701cc49f0759d2
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-10-17 14:55:45 +0300

    start working on logic behind watching files

--- AutoEncryptor/src/AutoEncryptor.java
+++ AutoEncryptor/src/AutoEncryptor.java
@@ -1,4 +1,37 @@
+import java.nio.file.*;
+import java.nio.file.WatchEvent.Kind;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+import static java.nio.file.LinkOption.*;
+
+import java.nio.file.attribute.*;
+import java.io.*;
+import java.util.*;
 
 public class AutoEncryptor {
+
+	public AutoEncryptor(Path dir, Path remoteDir) {
+		// TODO Auto-generated constructor stub
+	}
+
+	private void processEvents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void usage() {
+		System.out.println("Usage information:");
+	}
+
+	public static void main(String[] args) {
+		// parse arguments
+		if (args.length != 2)
+			usage();
+
+		// register directory and remote and process its events
+		Path dir = Paths.get(args[0]);
+		Path remoteDir = Paths.get(args[1]);
+		new AutoEncryptor(dir, remoteDir).processEvents();
+	}
 
 }
