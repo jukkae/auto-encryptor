a0123a0bc57c352ca7c0f89cf260072f4ad91548
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-11-04 12:17:05 +0200

    first working beta with hardcoded paths et cetera

--- AutoEncryptor/src/AutoEncryptor.java
+++ AutoEncryptor/src/AutoEncryptor.java
@@ -15,6 +15,11 @@ public class AutoEncryptor {
 	private Path remoteDir;
 	private Path directory; // TODO this is for testing, use directory registry!
 
+	@SuppressWarnings("unchecked")
+	static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+		return (WatchEvent<T>) event;
+	}
+
 	public AutoEncryptor(Path dir, Path remoteDir) throws IOException {
 		this.watcher = FileSystems.getDefault().newWatchService();
 		this.keys = new HashMap<WatchKey, Path>();
@@ -46,7 +51,82 @@ public class AutoEncryptor {
 
 	private void processEvents() {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("All systems are go!");
+
+		for (;;) {
+			WatchKey key;
+
+			try {
+				key = watcher.take();
+			} catch (InterruptedException x) {
+				System.out.println(x.getStackTrace());
+				return;
+			}
+
+			Path dir = keys.get(key);
+			if (dir == null) {
+				System.err.println("WatchKey not recognized!!");
+				continue;
+			}
+
+			for (WatchEvent<?> event : key.pollEvents()) {
+				Kind<?> kind = event.kind();
+
+				// TBD - provide example of how OVERFLOW event is handled
+				if (kind == OVERFLOW) {
+					continue;
+				}
+
+				// Context for directory entry event is the file name of entry
+				WatchEvent<Path> ev = cast(event);
+				Path name = ev.context();
+				Path child = dir.resolve(name);
+
+				// print out event
+				System.out.format("%s: %s\n", event.kind().name(), child);
+				if (kind == ENTRY_CREATE) {
+					// try executing axcrypt for watched directory
+					try {
+						System.out.println("Name: " + name);
+						System.out.println("Child: " + child);
+						encrypt();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+
+			// reset key and remove from set if directory no longer accessible
+			boolean valid = key.reset();
+			if (!valid) {
+				keys.remove(key);
+
+				// all directories are inaccessible
+				if (keys.isEmpty()) {
+					break;
+				}
+			}
+
+		}
+	}
+
+	private void encrypt() throws IOException {
+		String remote = remoteDir.toString();
+		String dir = directory.toString();
+		System.out.println("trying axcrypt, remote is: " + remote);
+		System.out.println("directory is: " + dir);
+
+		Process axCryptProcess = Runtime
+				.getRuntime()
+				.exec("C:\\Program Files\\Axantum\\Axcrypt\\AxCrypt -b 2 -e -k \"testi\" -z C:\\Users\\jukkae\\Documents\\actest\\secrets.txt");
+		InputStream rdiffStream = axCryptProcess.getInputStream();
+		Reader reader = new InputStreamReader(rdiffStream);
+		BufferedReader bReader = new BufferedReader(reader);
+		String nextLine = null;
+		while ((nextLine = bReader.readLine()) != null) {
+			System.out.println(nextLine);
+		}
 	}
 
 	public static void usage() {
